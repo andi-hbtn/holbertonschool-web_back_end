0456ecc2e15c745bcadb1921a3650a3481a98595
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-01-10 10:51:42 +0100

    changes

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
     return initialNumber = initialNumber + (expansion1989 ? expansion1989 : 89) + (expansion2019 ? expansion2019 : 19)
 }
