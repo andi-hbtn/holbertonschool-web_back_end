4396179da374610be431cd1b4f32ebff911ea1c4
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-01-10 15:33:35 +0100

    changes

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-    const values = [[...array1] + " " + [...array2] + "," + string.split("")]
+    const values = [[...array1] + " " + [...array2] + "," + [...string]]
     return values;
 }
