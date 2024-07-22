18213993398bec4d19ee3a55d58b22979c7bc410
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-07-22 13:19:12 +0200

    npx elint

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,36 +1,36 @@
 export default class Pricing {
-	constructor(amount, currency) {
-		this._amount = amount;
-		this._currency = currency;
-	}
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-	get amount() {
-		return this._amount;
-	}
+  get amount() {
+    return this._amount;
+  }
 
-	set amount(amount) {
-		if (typeof amount !== 'number') {
-			throw new Error('Code must be a number');
-		}
-		this._amount = amount;
-	}
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new Error('Code must be a number');
+    }
+    this._amount = amount;
+  }
 
-	get currency() {
-		return this._currency;
-	}
+  get currency() {
+    return this._currency;
+  }
 
-	set currency(newCurrency) {
-		if (typeof this._currency !== 'string') {
-			throw new Error('Name must be a string');
-		}
-		this._currency = newCurrency;
-	}
+  set currency(newCurrency) {
+    if (typeof this._currency !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._currency = newCurrency;
+  }
 
-	displayFullPrice() {
-		return `${this._amount} ${this._currency.name} (${this._currency._code})`;
-	}
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency._code})`;
+  }
 
-	static convertPrice(amount, currency) {
-		return amount * currency;
-	}
+  static convertPrice(amount, currency) {
+    return amount * currency;
+  }
 }
