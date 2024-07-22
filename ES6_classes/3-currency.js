03ee8655d202a87ce12aebb64bd0098959dbf0b7
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-07-22 11:56:01 +0200

    changes

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,26 +1,33 @@
 export default class Currency {
-	constructor(_name, _code) {
-		this._name = _name;
+	constructor(_code, _name) {
 		this._code = _code;
-	}
+		this._name = _name;
 
-	get name() {
-		return this._name;
-	}
-
-	set name(newName) {
-		this._name = newName;
 	}
 
 	get code() {
 		return this._code;
 	}
 
-	set code(newCode) {
-		this._code = newCode;
+	set code(code) {
+		if (typeof code !== 'string') {
+			throw new Error('Code must be a string');
+		}
+		this._code = code;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(name) {
+		if (typeof name !== 'string') {
+			throw new Error('Name must be a string');
+		}
+		this._name = name;
 	}
 
 	displayFullCurrency() {
-		return `${this._code} (${this._name})`;
+		return `${this._name} (${this._code})`;
 	}
 }
