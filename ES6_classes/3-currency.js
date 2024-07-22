f057d8e94f1c68974d159ae73e2fe8954ce15bd1
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-07-22 11:26:12 +0200

    currency

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+	constructor(_name, _code) {
+		this._name = _name;
+		this._code = _code;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(newName) {
+		this._name = newName;
+	}
+
+	get code() {
+		return this._code;
+	}
+
+	set code(newCode) {
+		this._code = newCode;
+	}
+
+	displayFullCurrency() {
+		return this._code + " " + this._name;
+	}
+}
