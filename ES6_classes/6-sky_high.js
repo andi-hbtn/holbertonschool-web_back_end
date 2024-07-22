8ee9a2a6b4b1d802017e9d39c0242f2f331e1e0b
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-07-22 14:59:29 +0200

    changes

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,26 +1,26 @@
 export default class SkyHighBuilding {
-	constructor(sqft, floors) {
-		this._sqft = sqft;
-		this._floors = floors;
-	}
+  constructor(sqft, floors) {
+    this._sqft = sqft;
+    this._floors = floors;
+  }
 
-	get sqft() {
-		return this._sqft
-	}
+  get sqft() {
+    return this._sqft;
+  }
 
-	set sqft(newSquare) {
-		return this._sqft = newSquare;
-	}
+  set sqft(newSquare) {
+    return this._sqft = newSquare;
+  }
 
-	get floors() {
-		return this._floors;
-	}
+  get floors() {
+    return this._floors;
+  }
 
-	set floors(newSquare) {
-		return this.floors = newSquare;
-	}
+  set floors(newSquare) {
+    return this.floors = newSquare;
+  }
 
-	evacuationWarningMessage() {
-		return `Evacuate slowly the ${this._floors} floors`;
-	}
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
