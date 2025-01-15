400d06a0115761a4c46577cf91cd565aa595a744
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-01-15 17:47:22 +0100

    changes

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,18 +5,10 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
 
   return Promise.allSettled(promises)
-    .then((results) => { return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      }
-      return {
-        status: result.status,
-        value: result.reason.toString(),
-      };
-    });
+    .then((results)=>{
+        return results.map((result)=>{
+            return result.status === 'fulfilled' ? { status: result.status,value: result.value }: { status: result.status, value: result.reason.toString() }
+        })
     })
-    .catch((error) => error );
+    .catch(error => error)
 }
