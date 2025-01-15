ba6abe1043ee84858c1960d8dfae05009c97c7b4
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-01-15 14:05:31 +0100

    final user

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,29 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName,lastName,filename){
+
+
+    const promises = [signUpUser(firstName,lastName),uploadPhoto(filename)];
+
+    Promise.allSettled(promises)
+    .then((results)=>{
+        return results.map((result)=>{
+            if(result.status === "fulfilled"){
+                return {
+                    status: result.status,
+                    value: result.value,
+                };
+            }else{
+                return {
+                    status: result.status,
+                    value: result.reason.toString(),
+                };
+            }
+        });
+    })
+    .catch((error)=>{
+        console.log("error----",error);
+        return error;
+    })
+}
