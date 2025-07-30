e2889f43f72ec5b5e279fa1835008507eee57202
andi-hbtn <andi.bevapi@gmail.com>
2025-07-30 10:12:59 +0000

    changed eslint extension

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     extends: [
         'airbnb-base',
-        'plugin:jest/all',
+        'plugin:jest/recommended',
     ],
     globals: {
         Atomics: 'readonly',
