ad7a1ba8dd845c661790a7cb9dcd6185d17f361a
andi-hbtn <andi.bevapi@holbertonschool>
2025-08-06 15:57:21 +0000

    klasat

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,19 +4,19 @@ export default class HolbertonCourse {
         if(typeof name === 'string' ){
             this._name = name;
         } else{
-            throw new Error("Name must be a string")
+            throw new Error('Name must be a string')
         }
 
         if(typeof length === 'number'){
             this._length = length;
         }else{
-            throw new Error("Length must be a number")
+            throw new Error('Length must be a number')
         }
         
         if(Array.isArray(students)){
             this._students = students;
         } else{
-            throw new Error("Students must be an array of strings")
+            throw new Error('Students must be an array of strings')
         }
     }
 
@@ -36,7 +36,7 @@ export default class HolbertonCourse {
         if(typeof name === 'string' ){
             return this._name = name;
         } else{
-            throw new Error("Name must be a string")
+            throw new Error('Name must be a string')
         }
         
     }
@@ -45,7 +45,7 @@ export default class HolbertonCourse {
         if(typeof length === 'number'){
             return this._length = length;
         }else{
-            throw new Error("Length must be a number")
+            throw new Error('Length must be a number')
         }
     }
 
@@ -53,7 +53,7 @@ export default class HolbertonCourse {
         if(Array.isArray(students)){
             return this._students = students;
         } else{
-            throw new Error("Students must be an array of strings")
+            throw new Error('Students must be an array of strings')
         }
         
     }
