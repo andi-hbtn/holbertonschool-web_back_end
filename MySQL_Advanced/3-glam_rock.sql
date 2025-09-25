8dcbd6caf42599850e1be414f896feda35d5ace2
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-25 14:29:03 +0200

    3-main

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,4 @@
--- List glam rock bands
-SELECT band_name, IFNULL(split, 2023) - formed as lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
+SELECT band_name, IFNULL(split, 2023) - formed + 1 as lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
