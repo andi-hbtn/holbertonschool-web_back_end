e0d34085ffd0cbb71aaa76a011f36dc48a15ab15
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-25 14:42:34 +0200

    changes

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,9 @@
 -- List glam rock bands and calculate lifespan
 SELECT band_name,
-       -- Nëse banda ka vit split, përdoret ai; në të kundërt përdoret 2023
-       IFNULL(split, 2023) - formed + 1 AS lifespan
+       CASE 
+           WHEN split IS NULL THEN 0
+           ELSE split - formed + 1
+       END AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
