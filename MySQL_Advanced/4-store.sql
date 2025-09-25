1b41f7cde613f689d28c4ba8b680e1950a102cc5
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-25 14:38:03 +0200

    changes

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
--- Decrease the quantity of items after an order
-CREATE TRIGGER decrease_quantity_trigger BEFORE INSERT ON orders
-FOR EACH ROW UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = NEW.item_name;
+-- List glam rock bands
+SELECT band_name, IFNULL(split, 2023) - formed + 1 AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
