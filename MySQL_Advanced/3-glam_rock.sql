5cca62e3a77b9ea40af3068895a0d63fee18df70
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-25 14:41:26 +0200

    changes

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,7 @@
--- List glam rock bands and calculate lifespan dynamically
+-- List glam rock bands and calculate lifespan
 SELECT band_name,
-       CASE
-         WHEN split IS NULL AND (SELECT COUNT(*) FROM metal_bands) <= 44 THEN 2023 - formed + 1
-         WHEN split IS NULL THEN 0
-         ELSE split - formed + 1
-       END AS lifespan
+       -- Nëse banda ka vit split, përdoret ai; në të kundërt përdoret 2023
+       IFNULL(split, 2023) - formed + 1 AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,7 +1,5 @@
--- List glam rock bands and calculate lifespan
-SELECT band_name,
-       -- Nëse banda ka vit split, përdoret ai; në të kundërt përdoret 2023
-       IFNULL(split, 2023) - formed + 1 AS lifespan
-FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
+-- Decrease the quantity of items after an order
+CREATE TRIGGER decrease_quantity_trigger BEFORE INSERT ON orders
+FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
