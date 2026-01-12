dfd1466390d38a34cea9eb48b9c46a7ad34540e4
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-01-12 18:55:07 +0100

    3-read file

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,33 +1,35 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-  let data;
+    let data;
 
-  try {
-    data = fs.readFileSync(filePath, 'utf-8');
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-
-  const lines = data.trim().split('\n');
-  const students = lines.slice(1);
-  console.log(`Number of students: ${students.length}`);
-
-  const fields = {};
-
-  students.forEach((line) => {
-    const [firstname, , , field] = line.split(',');
-    if (!fields[field]) {
-      fields[field] = [];
+    try {
+        data = fs.readFileSync(filePath, 'utf-8');
+    } catch (error) {
+        throw new Error('Cannot load the database');
     }
-    fields[field].push(firstname);
-  });
 
-  for (const field in fields) {
-    console.log(
-      `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
-    );
-  }
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1);
+
+    const fields = {};
+
+    console.log("students--", students);
+
+    students.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        console.log("firstname--", line);
+        if (!fields[field]) {
+            fields[field] = [];
+        }
+        fields[field].push(firstname);
+    });
+
+    for (const field in fields) {
+        // console.log(
+        //   `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+        // );
+    }
 }
 
 module.exports = countStudents;
---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises; // përdorim fs me Promise
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf-8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1).filter((line) => line.trim() !== '');
+      console.log(`Number of students: ${students.length}`);
+      const fields = {};
+
+      students.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      });
+
+      for (const field in fields) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
