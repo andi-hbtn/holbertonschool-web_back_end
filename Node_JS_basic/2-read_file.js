ba01fac3b3248c5d455a6a31c242ce1865013aa7
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-01-12 17:53:15 +0100

    changes

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+
+    let data;
+
+    try {
+        data = fs.readFileSync(filePath, 'utf-8');
+    } catch (error) {
+        throw new Error("Cannot load the database");
+    }
+
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1);
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+
+    students.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+
+        if (!fields[field]) {
+            fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+    });
+
+    for (const field in fields) {
+        console.log(
+            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+        );
+    }
+
+}
+
+module.exports = countStudents;
