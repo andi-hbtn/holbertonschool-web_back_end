e91f60c497675a576ebef3ea526917164dc5d7b5
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-01-30 18:25:58 +0100

    fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, string) {
+  if (!string || string.length === 0) {
+    return '';
+  }
   const values = [];
   for (const x of set.keys()) {
     if (x.includes(string)) {
