61fb8bfd60449e87b90d3652dddd37d0e49fc3b4
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-08-06 18:48:27 +0200

    task_1

--- ES6_promise/index.js
+++ ES6_promise/index.js
@@ -1,38 +1,48 @@
+// const myPromise = new Promise((resolve, reject) => {
+//     const success = { status: 200, message: "User is loged in" };
+//     const fail = { status: 404, message: "User is not loged in" };
+//     setTimeout(() => {
+//         return resolve(success)
+//     }, 2000);
+// });
+
+// const secondPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         return resolve("second-promise")
+//     }, 2000);
+// });
+// const thirdPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         return resolve("third-promise")
+//     }, 2000);
+// });
+
+
 const myPromise = new Promise((resolve, reject) => {
+    resolve("resolve");
 
-    const success = { status: 200, message: "User is loged in" };
-    const fail = { status: 404, message: "User is not loged in" };
-    setTimeout(() => {
-        return resolve(success)
-    }, 2000);
 });
 
-const secondPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        return resolve("second-promise")
-    }, 2000);
-});
-const thirdPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        return resolve("third-promise")
-    }, 2000);
-});
 
-console.log("2")
+const myNumber = 1;
+const myName = "john";
 
-async function resolvePromise() {
-    try {
-        const first = Promise.resolve(await myPromise);
-        const second = await secondPromise;
-        const third = await thirdPromise;
-        return first;
-    } catch (error) {
-        return error;
-    }
-}
+console.log(myNumber instanceof Promise)
 
-resolvePromise()
 
-console.log("1")
+// async function resolvePromise() {
+//     try {
+//         const first = Promise.resolve(await myPromise);
+//         const second = await secondPromise;
+//         const third = await thirdPromise;
+//         return first;
+//     } catch (error) {
+//         return error;
+//     }
+// }
 
-console.log("----", Promise.resolve("hello"))
+// resolvePromise()
+
+// console.log("1")
+
+// console.log("----", Promise.resolve("hello"))
