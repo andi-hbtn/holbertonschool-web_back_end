537e167a92443be19609f616c559235309a5c1f8
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-08-07 18:54:30 +0200

    task_6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName), uploadPhoto(filename)])
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((result) => {
       console.log(result);
     })
